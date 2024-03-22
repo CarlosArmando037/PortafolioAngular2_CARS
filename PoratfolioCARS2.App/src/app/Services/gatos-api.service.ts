63a4e021a44b6b05ed04f292f88d96d1b22b8ffc
carlosarmando <elramirosalas400@gmail.com>
2024-03-22 11:14:02 -0700

    finalizado

--- PoratfolioCARS2.App/src/app/Services/gatos-api.service.ts
+++ PoratfolioCARS2.App/src/app/Services/gatos-api.service.ts
@@ -6,10 +6,22 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GatosApiService {
+  private apikey="tu_api_key";
+  constructor(private http: HttpClient){}
+
+  getcat():Observable<any>{
+    return this.http.get<any>('https://api.thecatapi.com/v1/images/search',{
+      headers:{
+        "x-api-key":this.apikey
+      }
+    });
+  }
+
+  /*
   private ApiGatos = "https://dog.ceo/api/breeds/image/random";
   constructor(private http: HttpClient) { }
 
   public getData(): Observable<any>{
     return this.http.get<any>(this.ApiGatos);
-  }
+  }*/
 }
--- PoratfolioCARS2.App/src/app/app.component.ts
+++ PoratfolioCARS2.App/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { proyectosModel } from './Models/MisProyectosModel';
 import { ProyectosService } from './Services/mispoyectos.service';
 import { GatosApiService } from './Services/gatos-api.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,7 @@ import { GatosApiService } from './Services/gatos-api.service';
 })
 export class AppComponent 
 {
-  data: any[]=[];
+  datacat: any[]=[];
   imageUrl ="";
 
   title = 'PortafolioCARS.App';
@@ -23,18 +24,31 @@ export class AppComponent
   constructor(private proyectoservices : ProyectosService, private gatosApi : GatosApiService){}
     ngOnInit(): void {
       //esto es de la api de gatos
-      this.llenarData();
+      //this.llenarData();
 
       //esto es la api que cree
       this.proyectoservices.getDriver().subscribe((result: proyectosModel[])=>{
         this.proyectos =result;});
       console.log(this.proyectos[0])
+
+      this.getcatAleatorio();
   }
+  getcatAleatorio(): void{
+    this.gatosApi.getcat().subscribe((response: any)=>{
+      this.imageUrl = response[0].url;
+    });
+  }
+  obtenerimg(): void{
+    console.log("gato");
+    this.getcatAleatorio()
+  }
+
+  /*
   llenarData(){
     this.gatosApi.getData().subscribe(data =>{
       this.data = data;
       this.imageUrl = data.url;
       console.log(this.data)
     })
-  }
+  }*/
 }
